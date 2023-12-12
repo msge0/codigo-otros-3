11e1cda980006c75db7d8959f43d3e35ede476b8
geo flores <myssgeox@outlook.com>
2023-12-11 22:44:59 -0600

    cambios

--- filtro.js
+++ filtro.js
@@ -1,63 +1,59 @@
 // Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  { nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg" },
+  { nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg" },
+  { nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg" },
+  { nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg" },
+  { nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg" }
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+// se modifica el metodo para acceder al id del elemento div en dom
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+const list = document.getElementById("lista-de-productos");
+//se quita el . porque no es una clase y se esta accediendo al elemento input del dom con el metodo query
+const input = document.querySelector("input");
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+// Se define la función para mostrar los productos filtrados en el DOM
+const displayProductos = (productos) => {
 
-  li.appendChild(d)
+
+  for (let i = 0; i < productos.length; i++) {
+    //se declara la variable como element
+    var element = document.createElement("div");
+    element.classList.add("producto");
+
+    //se declara la variable como name 
+    var name = document.createElement("p");
+    name.classList.add("titulo");
+    name.textContent = productos[i].nombre;
+
+    //se declara la variable como image
+    var image = document.createElement("img");
+    image.setAttribute('src', productos[i].img);
+
+    element.appendChild(name);
+    element.appendChild(image);
+
+    list.appendChild(element);
+  }
 }
 
-displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
+//se crea la funcion para filtrar al presionar el boton 
 botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = input.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
-  
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
-    
-    var imagen = document.createElement("img");
-    imagen.setAttribute('src', productosFiltrados[i].img);
-  
-    d.appendChild(ti)
-    d.appendChild(imagen)
-  
-    li.appendChild(d)
-  }
+  const productosFiltrados = filtrado(productos, texto);
+  displayProductos(productosFiltrados);
 }
 
-const filtrado = (productos = [], texto) => {
+const filtrado = (productos, texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
 }  
